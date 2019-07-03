4d6a91510ca3fd1e7ac4fc71aff537c5fe8f4441
Jared NAIDOO <jnaidoo@c5r5s1.wethinkcode.co.za>
2019-07-03 17:08:58 +0200

    Final Push

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jnaidoo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 08:13:02 by jnaidoo           #+#    #+#             */
-/*   Updated: 2019/05/31 14:53:35 by jnaidoo          ###   ########.fr       */
+/*   Updated: 2019/07/03 16:32:28 by jnaidoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,25 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*a;
-	size_t	b;
-	size_t	c;
-	size_t	d;
+	int		c;
+	int		d;
 
 	if (!s1 || !s2)
 		return (NULL);
-	b = ft_strlen(s1) + ft_strlen(s2);
 	c = 0;
 	d = 0;
-	if (!(a = ft_memalloc(b + 1)))
+	if (!(a = ft_memalloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
 		return (NULL);
-	while (c < ft_strlen(s1))
+	while (s1[c] != '\0')
 	{
 		a[c] = s1[c];
 		c++;
 	}
-	while (d < ft_strlen(s2))
+	while (s2[d] != '\0')
 	{
-		a[c] = s2[d];
-		c++;
+		a[c + d] = s2[d];
 		d++;
 	}
-	a[b + 1] = '\0';
+	a[c + d] = '\0';
 	return (a);
 }
